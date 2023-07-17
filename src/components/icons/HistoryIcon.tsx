4b27e705fd218eee63ce1c2ae337da817935b9a5
SeungYn <kwls0407@gmail.com>
2023-07-17 13:24:55 +0900

    feat: 아이콘 컴포넌트 작성

---
+++ src/components/icons/HistoryIcon.tsx
@@ -0,0 +1,9 @@
+import { AiOutlineHistory } from 'react-icons/ai';
+import { IconProp } from './types';
+import { getIconSize } from '@/utils/size';
+
+type Prop = IconProp;
+
+export default function HistoryIcon({ size }: Prop) {
+  return <AiOutlineHistory style={{ fontSize: getIconSize(size) }} />;
+}
---
+++ src/components/icons/HomeIcon.tsx
@@ -0,0 +1,9 @@
+import { AiOutlineHome } from 'react-icons/ai';
+import { IconProp } from './types';
+import { getIconSize } from '@/utils/size';
+
+type Prop = IconProp;
+
+export default function HomeIcon({ size }: Prop) {
+  return <AiOutlineHome style={{ fontSize: getIconSize(size) }} />;
+}
