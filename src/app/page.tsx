80e53500a8861ab548c6ca9bdf52b68497353b27
SeungYn <kwls0407@gmail.com>
2023-07-19 01:07:18 +0900

    design: 미디어 쿼리 폰트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ReduceTimer } from '@/components/home';
 
 export default function Home() {
   return (
-    <div className='w-full h-full flex justify-center'>
+    <div className='w-full h-full flex items-center flex-col'>
       <h1 className={`text-5xl text-neutral-100 mt-8 text-center font-bold `}>
         하루를 <strong className='text-red-300'>어떻게</strong> <br />{' '}
         사용하셨나요?
