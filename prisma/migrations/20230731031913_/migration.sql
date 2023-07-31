5e5ee4e9eafc98e09ae83d109cf92f766ac4d946
SeungYn <kwls0407@gmail.com>
2023-07-31 15:02:28 +0900

    chore: dbURL 분리

---
+++ prisma/migrations/20230731031913_/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE `User` ADD COLUMN `account_email` VARCHAR(191) NULL,
+    ADD COLUMN `profile_image` VARCHAR(191) NULL,
+    ADD COLUMN `profile_nickname` VARCHAR(191) NULL;
