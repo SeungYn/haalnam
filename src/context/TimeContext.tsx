3031e41dc541a887d484e82fc61f65850d818ef3
SeungYn <kwls0407@gmail.com>
2023-08-18 21:43:01 +0900

    refactor: timeContext, useReducer로 리팩토링

--- src/context/TimeContext.tsx
+++ src/context/TimeContext.tsx
@@ -6,35 +6,70 @@ import {
   createContext,
   useCallback,
   useContext,
+  useReducer,
   useState,
 } from 'react';
 
 type TimeContextType = {
+  startTime: Date | undefined;
   status: Status;
+  subject: string;
 };
 
 type TimeActionContextType = {
   handleStatusToggle: () => void;
+  startTime: (startTime: Date, subject: string) => void;
 };
 
-const TimeContext = createContext<TimeContextType>({ status: 'END' });
+type TimeContextAction =
+  | { type: 'status'; payload: Status }
+  | { type: 'startTime'; payload: Date | undefined }
+  | { type: 'subject'; payload: string }
+  | { type: 'all'; payload: TimeContextType };
+
+const initialTimeContext: TimeContextType = {
+  status: 'END',
+  startTime: undefined,
+  subject: '',
+};
+
+const TimeContext = createContext<TimeContextType>(initialTimeContext);
+
 const TimeActionContext = createContext<TimeActionContextType>({
   handleStatusToggle: () => {},
+  startTime: (startTime, subject) => {},
 });
 
+function timeContextReducer(state: TimeContextType, action: TimeContextAction) {
+  switch (action.type) {
+    case 'status':
+      return { ...state, status: action.payload };
+    case 'subject':
+      return { ...state, subject: action.payload };
+    case 'startTime':
+      return { ...state, startTime: action.payload };
+    case 'all':
+      return { ...action.payload };
+    default:
+      return { ...state };
+  }
+}
+
 export default function TimeContextProvider({ children }: PropsWithChildren) {
-  const [status, setStatus] = useState<Status>('END');
+  const [state, dispatch] = useReducer(timeContextReducer, initialTimeContext);
 
   const handleStatusToggle = useCallback(() => {
-    setStatus((f) => {
-      if (f === 'START') return 'END';
-      return 'START';
-    });
+    if (state.status === 'START') dispatch({ type: 'status', payload: 'END' });
+    else dispatch({ type: 'status', payload: 'START' });
+  }, []);
+
+  const startTime = useCallback((startTime: Date, subject: string) => {
+    dispatch({ type: 'all', payload: { subject, startTime, status: 'START' } });
   }, []);
 
   return (
-    <TimeContext.Provider value={{ status }}>
-      <TimeActionContext.Provider value={{ handleStatusToggle }}>
+    <TimeContext.Provider value={state}>
+      <TimeActionContext.Provider value={{ handleStatusToggle, startTime }}>
         {children}
       </TimeActionContext.Provider>
     </TimeContext.Provider>
