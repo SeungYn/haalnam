b28bd66666ea776c423c165c1698f8fffd260fa9
SeungYn <kwls0407@gmail.com>
2023-09-18 15:38:24 +0900

    fix: [#6] timer 시작시 시간초 증가 할 때 setInterval로 변경

--- src/components/home/time/TimerSituation/TimerSituation.tsx
+++ src/components/home/time/TimerSituation/TimerSituation.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/common';
 import { TimeContextType } from '@/context/TimeContext';
 import { useRequestAnimationFrame } from '@/hooks/common';
 import { FrameSeconds, millsecondsToSeconds } from '@/utils/date';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 type Props = {
   onEndTime: () => void;
@@ -18,15 +18,21 @@ export default function TimerSituation({
 }: Props) {
   const [timer, setTimer] = useState(0);
 
-  useRequestAnimationFrame(() => {
-    setTimer(incrementTime);
-  });
+  // useRequestAnimationFrame(() => {
+  //   setTimer(incrementTime);
+  // });
+
+  useEffect(() => {
+    const interID = setInterval(() => setTimer(incrementTime), 1000);
+
+    return () => {
+      clearInterval(interID);
+    };
+  }, []);
 
   return (
     <div className='w-full max-w-3xl text-center text-yellow-100 text-4xl flex flex-col items-center gap-4'>
-      <h3 className='bg-main p-4 rounded-full w-full'>{`현재 ${subject}에 ${millsecondsToSeconds(
-        timer
-      )}초(원) 사용하셨습니다.`}</h3>
+      <h3 className='bg-main p-4 rounded-full w-full'>{`현재 ${subject}에 ${timer}초(원) 사용하셨습니다.`}</h3>
       <Button size='medium' onClick={() => onEndTime()}>
         타이머 종료하기
       </Button>
@@ -34,4 +40,4 @@ export default function TimerSituation({
   );
 }
 
-const incrementTime = (n: number) => parseInt(String(n + FrameSeconds));
+const incrementTime = (n: number) => parseInt(String(n + 1));
