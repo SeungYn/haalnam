bbc5ad8a1ad98fd6af3ed5b9dafc973a8425d9d1
SeungYn <kwls0407@gmail.com>
2024-07-19 01:53:59 +0900

    feat: calendar 글로벌 타입으로 추가

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,18 +2,24 @@ import { Time } from '@prisma/client';
 
 export {};
 declare global {
-  type CustomPath2D = {
-    path2D: Path2D;
-    rgba: RGBA;
-    colorPaletteIndex: number;
-    startTimeObj: Time;
-    endTimeObj: Time;
-  };
+	type CustomPath2D = {
+		path2D: Path2D;
+		rgba: RGBA;
+		colorPaletteIndex: number;
+		startTimeObj: Time;
+		endTimeObj: Time;
+	};
 
-  type RGBA = {
-    r: number;
-    g: number;
-    b: number;
-    a: number;
-  };
+	type RGBA = {
+		r: number;
+		g: number;
+		b: number;
+		a: number;
+	};
+
+	type Calendar = {
+		day: number;
+		date: Date;
+		accent: boolean;
+	};
 }
