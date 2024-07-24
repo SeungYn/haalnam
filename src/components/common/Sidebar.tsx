344c731b257c7662edc21afd020cfd1483f219fb
SeungYn <kwls0407@gmail.com>
2024-07-24 15:42:23 +0900

    refact: tailwind 프리티어 적용

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -3,14 +3,14 @@ import HomeIcon from '../icons/HomeIcon';
 import HistoryIcon from '../icons/HistoryIcon';
 
 export default function Sidebar() {
-  return (
-    <nav className='px-2 py-2 flex gap-2 bg-h_black  border-t border-h_gray sm:border sm:border-l-0 sm:rounded-tr-lg sm:rounded-br-lg sm:flex-col sm:py-4'>
-      <Link href={'/'}>
-        <HomeIcon size='medium' />
-      </Link>
-      <Link href={'/time'}>
-        <HistoryIcon size='medium' />
-      </Link>
-    </nav>
-  );
+	return (
+		<nav className="flex gap-2 border-t border-h_gray bg-h_black px-2 py-2 sm:flex-col sm:rounded-br-lg sm:rounded-tr-lg sm:border sm:border-l-0 sm:py-4">
+			<Link href={'/'}>
+				<HomeIcon size="medium" />
+			</Link>
+			<Link href={'/time'}>
+				<HistoryIcon size="medium" />
+			</Link>
+		</nav>
+	);
 }
