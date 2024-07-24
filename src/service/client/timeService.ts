b4f259ab1efcbfdb6d5149b3f8cd185d60c7d0e6
SeungYn <kwls0407@gmail.com>
2024-07-24 15:38:49 +0900

    feat: 날짜별로 time을 가져오는 클라이언트 서비스 코드 추가

--- src/service/client/timeService.ts
+++ src/service/client/timeService.ts
@@ -1,49 +1,60 @@
 import { AxiosInstance } from 'axios';
 import {
-  GetPersonalTodayTimeResponse,
-  PostTimeRequest,
-  PostTimeResponse,
+	GetPersonalTodayTimeResponse,
+	PostTimeRequest,
+	PostTimeResponse,
 } from '../types/time';
 
 export default class TimeService {
-  constructor(private axios: AxiosInstance) {}
+	constructor(private axios: AxiosInstance) {}
 
-  // async postTime({ time, subject, status }: PostTimeRequest) {
-  //   const formData = new FormData();
+	// async postTime({ time, subject, status }: PostTimeRequest) {
+	//   const formData = new FormData();
 
-  //   formData.append('subject', subject);
-  //   formData.append('status', status);
-  //   formData.append('time', String(time));
+	//   formData.append('subject', subject);
+	//   formData.append('status', status);
+	//   formData.append('time', String(time));
 
-  //   const { data } = await this.axios.post<PostTimeResponse>(
-  //     '/api/time',
-  //     formData
-  //   );
+	//   const { data } = await this.axios.post<PostTimeResponse>(
+	//     '/api/time',
+	//     formData
+	//   );
 
-  //   return data;
-  // }
+	//   return data;
+	// }
 
-  async postTime({ time, subject, status }: PostTimeRequest) {
-    const formData = new FormData();
+	async postTime({ time, subject, status }: PostTimeRequest) {
+		const formData = new FormData();
 
-    formData.append('subject', subject);
-    formData.append('status', status);
-    formData.append('time', String(time));
+		formData.append('subject', subject);
+		formData.append('status', status);
+		formData.append('time', String(time));
 
-    const res = await fetch('http://localhost:3000/api/time', {
-      method: 'POST',
-      body: formData,
-      keepalive: true,
-    });
-    const data = await res.json();
+		const res = await fetch('http://localhost:3000/api/time', {
+			method: 'POST',
+			body: formData,
+			keepalive: true,
+		});
+		const data = await res.json();
 
-    return data;
-  }
+		return data;
+	}
 
-  async getPersonalTodayTime() {
-    const { data } = await this.axios.get<GetPersonalTodayTimeResponse>(
-      '/api/time'
-    );
-    return data;
-  }
+	async getPersonalTodayTime() {
+		const { data } =
+			await this.axios.get<GetPersonalTodayTimeResponse>('/api/time');
+		return data;
+	}
+
+	async getTimesByDate(date: Date) {
+		const y = date.getFullYear();
+		const m = date.getMonth();
+		const d = date.getDate();
+		const params = encodeURIComponent(`${y}/${m}/${d}`);
+		const { data } = await this.axios.get<GetPersonalTodayTimeResponse>(
+			'/api/time/' + params
+		);
+
+		return data;
+	}
 }
