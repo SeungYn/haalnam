3931de8fb1c745594e9eaeb3692b45f03bf6cdca
SeungYn <kwls0407@gmail.com>
2024-07-24 15:45:31 +0900

    feat: 선택된 날짜를 기준으로 현재 날짜면 form을 아니면 날짜배너를 조건부렌더링

--- src/container/time/TimeFormContainer.tsx
+++ src/container/time/TimeFormContainer.tsx
@@ -1,30 +1,35 @@
 'use client';
 
+import DateBanner from '@/components/date/DateBanner';
 import TimeForm from '@/components/home/time/TimeForm.tsx/TimeForm';
 import { useTimeActionContext, useTimeContext } from '@/context/TimeContext';
 import { usePostEndTime, usePostStartTime } from '@/hooks/api/time';
 import { PostTimeRequest } from '@/service/types/time';
+import { useSelectedDateStore } from '@/store/dateStore';
+import { isCurrentDay } from '@/utils/date';
 import { Status } from '@prisma/client';
 
 export default function TimeFormContainer() {
-  const { status, subject } = useTimeContext();
-  const { handleStartTime, handleEndTime } = useTimeActionContext();
-  const { mutate } = usePostStartTime({ handleStartTime });
-  const { mutate: mutateTimeEnd } = usePostEndTime({ handleEndTime });
+	const { status, subject } = useTimeContext();
+	const { handleStartTime, handleEndTime } = useTimeActionContext();
+	const { mutate } = usePostStartTime({ handleStartTime });
+	const { mutate: mutateTimeEnd } = usePostEndTime({ handleEndTime });
+	const { selectedDate } = useSelectedDateStore();
 
-  const onStart = ({ subject, time, status }: PostTimeRequest) => {
-    mutate({ subject, time, status });
-  };
+	const onStart = ({ subject, time, status }: PostTimeRequest) => {
+		mutate({ subject, time, status });
+	};
 
-  const onEndTime = () => {
-    mutateTimeEnd({
-      subject,
-      status: Status.END,
-      time: new Date(),
-    });
-  };
+	const onEndTime = () => {
+		mutateTimeEnd({
+			subject,
+			status: Status.END,
+			time: new Date(),
+		});
+	};
 
-  if (status === 'START') return <></>;
+	if (status === 'START') return <></>;
 
-  return <TimeForm onStart={onStart} onEndTime={onEndTime} />;
+	if (!isCurrentDay(selectedDate)) return <DateBanner time={selectedDate} />;
+	return <TimeForm onStart={onStart} onEndTime={onEndTime} />;
 }
