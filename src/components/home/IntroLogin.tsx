a29377493540f90b4d111cae62bed0ef08f13bf9
SeungYn <kwls0407@gmail.com>
2024-07-24 16:26:31 +0900

    feat: 로그인 버튼 kakao로 디폴트 값 명시

--- src/components/home/IntroLogin.tsx
+++ src/components/home/IntroLogin.tsx
@@ -2,25 +2,21 @@
 
 import Image from 'next/image';
 import kakaoImage from '@/assets/kakao/kakao_login_medium_wide.png';
-import { signIn, useSession } from 'next-auth/react';
+import { signIn } from 'next-auth/react';
 import { Button } from '../common';
 
 export function IntroLogin() {
-  const session = useSession();
-  //console.log(session?.data?.user);
-
-  const onClick = () => {
-    console.log('click');
-    signIn();
-  };
-  return (
-    <>
-      <h3 className='mt-6 text-3xl text-white'>
-        로그인해서 시간을 알차게 사용하세요!!
-      </h3>
-      <Button className='mt-4' onClick={onClick}>
-        <Image src={kakaoImage} alt='kakao login' />
-      </Button>
-    </>
-  );
+	const onClick = () => {
+		signIn('kakao', { callbackUrl: '/' });
+	};
+	return (
+		<>
+			<h3 className="mt-6 text-3xl text-white">
+				로그인해서 시간을 알차게 사용하세요!!
+			</h3>
+			<Button className="mt-4 border-none" onClick={onClick}>
+				<Image src={kakaoImage} alt="kakao login" />
+			</Button>
+		</>
+	);
 }
