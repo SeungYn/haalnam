c673917740a41ae67e6ea3f9fcd02e6b637600ea
SeungYn <kwls0407@gmail.com>
2024-07-24 15:37:16 +0900

    feat: 날짜별로 하루 기록 가져오는 api 추가

---
+++ src/app/api/time/[date]/route.ts
@@ -0,0 +1,48 @@
+import { auth } from '@/lib/auth';
+import client from '@/lib/db';
+import { getFullDate, getNowYYYY_MM_DD } from '@/utils/date';
+import { NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(
+	request: NextRequest,
+	{ params: { date } }: { params: { date: any } }
+) {
+	const session = await auth();
+
+	//console.log('postsession', session);
+	if (!session)
+		return new NextResponse('Authentication Error ee', { status: 401 });
+	const { id } = session.user;
+
+	const [y, m, d] = date.split('/').map(Number);
+
+	// UTC 기준 시간 설정
+	const day = getFullDate(y, m, d);
+
+	const tomorrow = getFullDate(y, m, d + 1);
+	console.log(y, m, d);
+	console.log(day, tomorrow);
+	//console.log( day, tomorrow, id);
+	let times;
+	try {
+		times = await client.time.findMany({
+			where: {
+				time: {
+					gte: day,
+					lte: tomorrow,
+				},
+				userId: id,
+			},
+			orderBy: {
+				time: 'asc',
+			},
+		});
+	} catch (e) {
+		console.error(e);
+	}
+
+	//console.log(times);
+
+	return NextResponse.json(times, { status: 200 });
+}
--- src/app/api/time/route.ts
+++ src/app/api/time/route.ts
@@ -6,89 +6,89 @@ import { auth } from '@/lib/auth';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getLatestTime } from '@/service/server/timeServerService';
 
-export async function POST(request: NextRequest, response: NextResponse) {
-  const session = await auth();
+export async function POST(request: NextRequest) {
+	const session = await auth();
 
-  if (!session)
-    return new NextResponse('Authentication Error ee', { status: 401 });
-  const { id } = session.user;
+	if (!session)
+		return new NextResponse('Authentication Error ee', { status: 401 });
+	const { id } = session.user;
 
-  const formData = await request.formData();
-  const subject = formData.get('subject') as string;
-  const status = formData.get('status') as Status;
-  const time = formData.get('time');
+	const formData = await request.formData();
+	const subject = formData.get('subject') as string;
+	const status = formData.get('status') as Status;
+	const time = formData.get('time');
 
-  // time를 하나 만들때 user 상태도 변경시켜 줘야함.
-  const res = client.$transaction(async () => {
-    let date = getNowDate();
+	// time를 하나 만들때 user 상태도 변경시켜 줘야함.
+	const res = client.$transaction(async () => {
+		let date = getNowDate();
 
-    if (status === 'END') {
-      const recentTime = await getLatestTime(id);
-      const now = getNowDate();
+		if (status === 'END') {
+			const recentTime = await getLatestTime(id);
+			const now = getNowDate();
 
-      // 만약 자정을 넘길경우 23:59:59 시간으로 처리
-      if (recentTime && recentTime?.time.getDate() < now.getDate()) {
-        date = new Date(
-          `${recentTime.time.toISOString().split('T')[0]}T23:59:59Z`
-        );
-      }
-    }
+			// 만약 자정을 넘길경우 23:59:59 시간으로 처리
+			if (recentTime && recentTime?.time.getDate() < now.getDate()) {
+				date = new Date(
+					`${recentTime.time.toISOString().split('T')[0]}T23:59:59Z`
+				);
+			}
+		}
 
-    const post = await client.time.create({
-      data: {
-        userId: id,
-        status: status,
-        subject: subject,
-        time: getNowDate(),
-      },
-    });
-    await client.user.update({
-      where: { id },
-      data: {
-        timer_status: status,
-      },
-    });
+		const post = await client.time.create({
+			data: {
+				userId: id,
+				status: status,
+				subject: subject,
+				time: getNowDate(),
+			},
+		});
+		await client.user.update({
+			where: { id },
+			data: {
+				timer_status: status,
+			},
+		});
 
-    return post;
-  });
-  console.log(time);
-  // 클라에서 보낸 time은 그대로 보내야됨 UTC + 9 이기 때문
-  //console.log(res);
-  return NextResponse.json({ ...res, time }, { status: 200 });
+		return post;
+	});
+	console.log(time);
+	// 클라에서 보낸 time은 그대로 보내야됨 UTC + 9 이기 때문
+	//console.log(res);
+	return NextResponse.json({ ...res, time }, { status: 200 });
 }
 
 export async function GET(request: NextApiRequest, res: NextApiResponse) {
-  const session = await auth();
-  //console.log('postsession', session);
-  if (!session)
-    return new NextResponse('Authentication Error ee', { status: 401 });
-  const { id } = session.user;
+	const session = await auth();
+	//console.log('postsession', session);
+	if (!session)
+		return new NextResponse('Authentication Error ee', { status: 401 });
+	const { id } = session.user;
 
-  // UTC 기준 시간 설정
-  const today = getNowYYYY_MM_DD();
+	// UTC 기준 시간 설정
+	const today = getNowYYYY_MM_DD();
 
-  const tomorrow = new Date(
-    getNowYYYY_MM_DD().setDate(getNowYYYY_MM_DD().getDate() + 1)
-  );
+	const tomorrow = new Date(
+		getNowYYYY_MM_DD().setDate(getNowYYYY_MM_DD().getDate() + 1)
+	);
 
-  //console.log(new Date(), today, tomorrow, id);
-  let times;
-  try {
-    times = await client.time.findMany({
-      where: {
-        time: {
-          gte: today,
-          lte: tomorrow,
-        },
-        userId: id,
-      },
-      orderBy: {
-        time: 'asc',
-      },
-    });
-  } catch (e) {
-    console.error(e);
-  }
+	//console.log(new Date(), today, tomorrow, id);
+	let times;
+	try {
+		times = await client.time.findMany({
+			where: {
+				time: {
+					gte: today,
+					lte: tomorrow,
+				},
+				userId: id,
+			},
+			orderBy: {
+				time: 'asc',
+			},
+		});
+	} catch (e) {
+		console.error(e);
+	}
 
-  return NextResponse.json(times, { status: 200 });
+	return NextResponse.json(times, { status: 200 });
 }
