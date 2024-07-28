01f01287a4a3c7242d464294eada25de03824862
SeungYn <kwls0407@gmail.com>
2024-07-28 10:40:33 +0900

    design: 사이드바 브레이크 포인트 수정

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -4,7 +4,7 @@ import HistoryIcon from '../icons/HistoryIcon';
 
 export default function Sidebar() {
 	return (
-		<nav className="flex gap-2 border-t border-h_gray bg-h_black px-2 py-2 sm:flex-col sm:rounded-br-lg sm:rounded-tr-lg sm:border sm:border-l-0 sm:py-4">
+		<nav className="flex gap-2 border-t border-h_gray bg-h_black px-2 py-2 md:flex-col md:rounded-br-lg md:rounded-tr-lg md:border md:border-l-0 md:py-4">
 			<Link href={'/'}>
 				<HomeIcon size="medium" />
 			</Link>
