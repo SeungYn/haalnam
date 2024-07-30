f7d7bf8a4f4c9c58aaf15ffcff1cbea5d0051418
SeungYn <kwls0407@gmail.com>
2024-07-30 23:39:38 +0900

    fix: 타임관련 suspense fallback ui 추가

--- src/container/time/TimeChartContainer.tsx
+++ src/container/time/TimeChartContainer.tsx
@@ -2,12 +2,13 @@
 
 import SSRSuspense from '@/components/common/SSRSuspense';
 import TimeChart from '@/components/home/time/chart/TimeChart/TimeChart';
+import TimeChartSkeleton from '@/components/home/time/chart/TimeChart/TimeChartSkeleton';
 import { useGetTimesByDate } from '@/hooks/api/time';
 import { useSelectedDateStore } from '@/store/dateStore';
 
 export default function TimeChartContainer() {
 	return (
-		<SSRSuspense fallback={<div>로딩중</div>}>
+		<SSRSuspense fallback={<TimeChartSkeleton />}>
 			<TimeChartSuspense />
 		</SSRSuspense>
 	);
@@ -18,7 +19,5 @@ function TimeChartSuspense() {
 
 	const { data } = useGetTimesByDate(selectedDate, true); //useGetPersonalTodayTime(true);
 
-	if (!data) return <></>;
-
-	return <TimeChart times={data} />;
+	return <TimeChart times={data!} />;
 }
