ac5159a0f23d01e5292f97956352a0ae51e3de45
SeungYn <kwls0407@gmail.com>
2024-07-31 16:24:51 +0900

    feat: Inter 폰트 추가

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,7 +1,14 @@
+import { Inter } from 'next/font/google';
 import localFont from 'next/font/local';
 
 export const sincerity = localFont({
-  src: './Sincerity.ttf',
-  variable: '--font-sincVariable',
-  display: 'swap',
+	src: './Sincerity.ttf',
+	variable: '--font-sincVariable',
+	display: 'swap',
+});
+
+export const interFont = Inter({
+	subsets: ['latin'],
+	variable: '--font-inter',
+	display: 'swap',
 });
