0c83c73f4d852c6e04b9f0bea4d7e8f6243efae7
SeungYn <kwls0407@gmail.com>
2024-08-03 00:45:05 +0900

    feat: 클라이언트 useService 로직 추가

--- src/service/client/index.ts
+++ src/service/client/index.ts
@@ -1,8 +1,10 @@
 import { axiosInstance } from '@/network/http';
 import TimeService from './timeService';
+import UserService from './userService';
 
 const service = {
-  time: new TimeService(axiosInstance),
+	time: new TimeService(axiosInstance),
+	user: new UserService(axiosInstance),
 };
 
 export default service;
