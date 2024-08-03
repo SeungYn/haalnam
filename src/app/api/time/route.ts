78f1f9d092a5e42b9471a61caf32ea73046831ac
SeungYn <kwls0407@gmail.com>
2024-08-03 16:03:15 +0900

    feat: 타이머 api 에러처리 추가

--- src/app/api/time/route.ts
+++ src/app/api/time/route.ts
@@ -5,6 +5,7 @@ import { Status, User } from '@prisma/client';
 import { auth } from '@/lib/auth';
 import { getLatestTime } from '@/service/server/timeServerService';
 import { checkUser } from '@/service/server/userServerService';
+import { ExceptionRes } from '@/utils/exception';
 
 export async function POST(request: NextRequest) {
 	const session = await auth();
@@ -14,7 +15,7 @@ export async function POST(request: NextRequest) {
 		user = await checkUser(session?.user?.id, session);
 	} catch {
 		return NextResponse.json(
-			{ message: '유저 정보가 올바르지 않습니다.' },
+			new ExceptionRes('유저 정보가 올바르지 않습니다.'),
 			{ status: 401 }
 		);
 	}
@@ -27,42 +28,51 @@ export async function POST(request: NextRequest) {
 	const time = formData.get('time');
 
 	// time를 하나 만들때 user 상태도 변경시켜 줘야함.
-	const res = await client.$transaction(async () => {
-		let date = getNowDate();
+	try {
+		const res = await client.$transaction(async () => {
+			let date = getNowDate();
 
-		if (timer_status === 'END') {
-			const recentTime = await getLatestTime(id);
-			const now = getNowDate();
+			if (timer_status === 'END') {
+				const recentTime = await getLatestTime(id);
+				const now = getNowDate();
 
-			// 만약 자정을 넘길경우 23:59:59 시간으로 처리
-			if (recentTime && recentTime?.time.getDate() < now.getDate()) {
-				date = new Date(
-					`${recentTime.time.toISOString().split('T')[0]}T23:59:59Z`
-				);
+				// 만약 자정을 넘길경우 23:59:59 시간으로 처리
+				if (recentTime && recentTime?.time.getDate() < now.getDate()) {
+					date = new Date(
+						`${recentTime.time.toISOString().split('T')[0]}T23:59:59Z`
+					);
+				}
 			}
-		}
 
-		const post = await client.time.create({
-			data: {
-				userId: id,
-				status: next_timer_status,
-				subject: subject,
-				time: getNowDate(),
-			},
-		});
-		await client.user.update({
-			where: { id },
-			data: {
-				timer_status: next_timer_status,
-			},
+			const post = await client.time.create({
+				data: {
+					userId: id,
+					status: next_timer_status,
+					subject: subject,
+					time: getNowDate(),
+				},
+			});
+			await client.user.update({
+				where: { id },
+				data: {
+					timer_status: next_timer_status,
+				},
+			});
+
+			return post;
 		});
 
-		return post;
-	});
+		return NextResponse.json({ ...res, time }, { status: 200 });
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json(
+			new ExceptionRes('에러가 발생했습니다. 다시 시도해주세요'),
+			{ status: 500 }
+		);
+	}
 
 	// 클라에서 보낸 time은 그대로 보내야됨 UTC + 9 이기 때문
 	// console.log('res', res);
-	return NextResponse.json({ ...res, time }, { status: 200 });
 }
 
 export async function GET(request: NextRequest) {
