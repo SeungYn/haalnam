e079e4873661374846787afa25401c2fe49a13e9
SeungYn <kwls0407@gmail.com>
2024-08-03 15:44:07 +0900

    feat: 리액트 쿼리 에러타입 추가 및 에러 콜백 추가

--- src/lib/queryClient.ts
+++ src/lib/queryClient.ts
@@ -1,14 +1,27 @@
-import { QueryClientConfig } from '@tanstack/react-query';
+import { QueryCache, QueryClientConfig } from '@tanstack/react-query';
+import { signOut } from 'next-auth/react';
 
 export const queryConfig: QueryClientConfig = {
-  defaultOptions: {
-    queries: {
-      retry: 0,
-      staleTime: 600000,
-      cacheTime: 900000,
-      refetchOnMount: true,
-      refetchOnReconnect: false,
-      refetchOnWindowFocus: false,
-    },
-  },
+	queryCache: new QueryCache({
+		onError: (error: unknown) => {
+			if (isReactQueryError(error)) {
+				signOut();
+			}
+			return;
+		},
+	}),
+	defaultOptions: {
+		queries: {
+			retry: 0,
+			staleTime: 600000,
+			cacheTime: 900000,
+			refetchOnMount: true,
+			refetchOnReconnect: false,
+			refetchOnWindowFocus: false,
+		},
+	},
 };
+
+function isReactQueryError(error: unknown): error is ReactQueryError {
+	return typeof error === 'object' && error !== null && 'status' in error;
+}
