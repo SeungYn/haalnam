08b3d45332f9a8fa90d8c45d75dc1ba64d1a3fdc
SeungYn <kwls0407@gmail.com>
2024-08-03 15:42:05 +0900

    feat: axios response interceptor 작성

--- src/network/http.ts
+++ src/network/http.ts
@@ -1,7 +1,18 @@
 import { whereHost } from '@/utils/util';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 export const axiosInstance = axios.create({
 	baseURL: whereHost(),
 	withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+	(res) => res,
+	(error: AxiosError) => {
+		const { response } = error;
+		const data: { [key: string]: any } = response?.data!;
+		let errorMsg = data.message || '에러가 발생했습니다.';
+
+		return Promise.reject({ message: errorMsg, status: error.status });
+	}
+);
