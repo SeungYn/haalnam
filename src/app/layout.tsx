49a698fc28977676448eca4b6471d4930b8e4650
SeungYn <kwls0407@gmail.com>
2024-08-04 14:54:46 +0900

    design: body wrapper 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ import ReactQueryContext from '@/context/ReactQueryContext';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import TimeContextProvider from '@/context/TimeContext';
 import CheckUnloadHOC from '@/hoc/CheckUnloadHOC';
+import ReactToastContainer from '@/components/common/toast/ReactToastContainer';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 export const metadata: Metadata = {
 	title: {
@@ -29,13 +31,16 @@ export default function RootLayout({
 				<AuthContext>
 					<TimeContextProvider>
 						<CheckUnloadHOC>
-							<body className="flex flex-col-reverse gap-4 bg-h_black text-white md:flex-row">
-								<Sidebar />
-								<main className="flex-1 overflow-y-auto">
-									<div className="mx-auto h-full min-h-full w-full max-w-screen-xl px-4 md:px-0">
-										{children}
-									</div>
-								</main>
+							<body>
+								<div className="flex h-full w-full flex-col-reverse gap-4 bg-h_black text-white md:flex-row">
+									<Sidebar />
+									<main className="flex-1 overflow-y-auto">
+										<div className="mx-auto h-full min-h-full w-full max-w-screen-xl px-4 md:px-0">
+											{children}
+										</div>
+									</main>
+								</div>
+								<ReactToastContainer />
 								<ReactQueryDevtools />
 							</body>
 						</CheckUnloadHOC>
