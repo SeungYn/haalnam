5fe70b798d7f53f9cb01c7dfedc0c5cf974648e2
SeungYn <kwls0407@gmail.com>
2024-08-13 22:26:45 +0900

    feat: 시간 추가 팝업 버튼 추가

--- src/components/home/time/TimeForm.tsx/TimeForm.tsx
+++ src/components/home/time/TimeForm.tsx/TimeForm.tsx
@@ -1,16 +1,29 @@
 'use client';
 
 import { Button } from '@/components/common';
+import { IoAddCircleIcon } from '@/components/icons';
 import { PostTimeRequest } from '@/service/types/time';
 import { Status } from '@prisma/client';
-import { ChangeEvent, FormEventHandler, useEffect, useState } from 'react';
+import {
+	ChangeEvent,
+	Dispatch,
+	FormEventHandler,
+	SetStateAction,
+	useEffect,
+	useState,
+} from 'react';
 
 type Props = {
 	onStart: ({ subject, time, status }: PostTimeRequest) => void;
 	onEndTime: () => void;
+	onAddPopUpOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-export default function TimeForm({ onStart, onEndTime }: Props) {
+export default function TimeForm({
+	onStart,
+	onEndTime,
+	onAddPopUpOpen,
+}: Props) {
 	const [timeTitle, setTimeTitle] = useState('');
 
 	const onChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -30,18 +43,31 @@ export default function TimeForm({ onStart, onEndTime }: Props) {
 	};
 
 	return (
-		<form
-			onSubmit={onSubmit}
-			className="flex w-full flex-col items-center gap-4"
-		>
-			<input
-				type="text"
-				className="w-full max-w-2xl rounded-[100px] border-2 border-h_light_black bg-h_black px-7 py-2 text-2xl focus:border-white"
-				onChange={onChange}
-				value={timeTitle}
-				placeholder="주제 ex: 운동하기"
-			/>
-			<Button size="medium">타이머 시작하기</Button>
-		</form>
+		<>
+			<form
+				onSubmit={onSubmit}
+				className="flex w-full flex-col items-center gap-4"
+			>
+				<input
+					type="text"
+					className="w-full max-w-2xl rounded-[100px] border-2 border-h_light_black bg-h_black px-7 py-2 text-2xl focus:border-white"
+					onChange={onChange}
+					value={timeTitle}
+					placeholder="주제 ex: 운동하기"
+				/>
+				<div className="flex gap-2">
+					<Button size="medium">타이머 시작하기</Button>
+					<Button
+						type="button"
+						size="medium"
+						className="rounded-full border-none !p-0"
+						onClick={() => onAddPopUpOpen(true)}
+						title="시간 추가하기"
+					>
+						<IoAddCircleIcon size="large" />
+					</Button>
+				</div>
+			</form>
+		</>
 	);
 }
