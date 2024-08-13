aa5d29627939fbe035424f034373f7c8c39b2b37
SeungYn <kwls0407@gmail.com>
2024-08-13 11:23:40 +0900

    feat: 타임 페이지 전용 팝업 추가

---
+++ src/components/home/time/TimePopUp/TimePopUp.tsx
@@ -0,0 +1,45 @@
+'use client';
+import { Dispatch, PropsWithChildren, SetStateAction, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+
+type Props = {
+	isMounting: boolean;
+	isOpen: boolean;
+	setIsOpen: Dispatch<SetStateAction<boolean>>;
+	setIsMounting: Dispatch<SetStateAction<boolean>>;
+};
+
+export default function TimePopUp({
+	isMounting,
+	isOpen,
+	setIsOpen,
+	setIsMounting,
+	children,
+}: PropsWithChildren<Props>) {
+	// useEffect(() => {
+	// 	if (!isOpen) return;
+
+	// 	let isFire = false;
+	// 	history.pushState(null, '', location.href);
+	// 	const backEvent = () => {
+	// 		isFire = true;
+	// 		setIsMounting(false);
+	// 	};
+	// 	window.addEventListener('popstate', backEvent);
+	// 	return () => {
+	// 		if (isOpen && !isFire) history.back();
+	// 		window.removeEventListener('popstate', backEvent);
+	// 	};
+	// }, [isOpen]);
+
+	if (!isOpen) return null;
+
+	return createPortal(
+		<section
+			className={`absolute top-0 z-[100] w-full grow bg-h_black px-4 transition-all md:absolute md:px-0 ${isMounting ? 'top-0' : 'top-full'}`}
+		>
+			{children}
+		</section>,
+		document.querySelector('#time-portal')!
+	);
+}
