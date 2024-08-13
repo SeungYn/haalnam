9aad517d6c722bcc610babf06238796f0c1589b2
SeungYn <kwls0407@gmail.com>
2024-08-13 22:15:39 +0900

    feat: 시간 추가 request 타입 추가

--- src/service/types/time.ts
+++ src/service/types/time.ts
@@ -4,3 +4,14 @@ export type PostTimeRequest = Omit<Time, 'id' | 'userId'>;
 export type PostTimeResponse = Time;
 
 export type GetPersonalTodayTimeResponse = Time[];
+
+export type PostAddTimerRequest = {
+	subject: string;
+	date: {
+		y: number;
+		m: number;
+		d: number;
+	};
+	startTime: { hours: number; minutes: number; seconds: number };
+	endTime: { hours: number; minutes: number; seconds: number };
+};
