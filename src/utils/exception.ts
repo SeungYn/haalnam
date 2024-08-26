7bca20cd8014184230f18303fa5d9ae6df43e818
SeungYn <kwls0407@gmail.com>
2024-08-26 16:40:31 +0900

    feat-server: 에러 유틸 추가

--- src/utils/exception.ts
+++ src/utils/exception.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from 'next/server';
+
 export class CustomException extends Error {
 	message;
 	code?: number;
@@ -21,3 +23,23 @@ export const ExceptionCode = {
 	TimeReset: 0,
 	TimeContinue: 1,
 } as const;
+
+type ErrorConfig = {
+	status: number;
+	message: string;
+};
+
+export enum ErrorCode {
+	BadRequest = '',
+}
+
+export function handleError(e: any) {
+	console.error(e);
+	if (e instanceof CustomException) {
+		return NextResponse.json(new ExceptionRes(e.message), { status: 400 });
+	}
+
+	return NextResponse.json(new ExceptionRes('에러가 발생했습니다'), {
+		status: 500,
+	});
+}
