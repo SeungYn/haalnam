3d435a5e867d33d58fe1d18783bf17f30c882e1a
SeungYn <kwls0407@gmail.com>
2024-08-26 19:00:13 +0900

    feat-client: 계획관련 api fetch 서비스 코드 추가

--- src/service/client/index.ts
+++ src/service/client/index.ts
@@ -1,9 +1,11 @@
 import { axiosInstance } from '@/network/http';
 import TimeService from './timeService';
 import UserService from './userService';
+import PlanService from './planService';
 
 const service = {
 	time: new TimeService(axiosInstance),
+	plan: new PlanService(axiosInstance),
 	user: new UserService(axiosInstance),
 };
 
---
+++ src/service/client/planService.ts
@@ -0,0 +1,68 @@
+import { AxiosInstance } from 'axios';
+import { PlanPageResponse, PostPlanRequest } from '../types/plan';
+import { Plan, PlanPage } from '@prisma/client';
+
+export default class PlanService {
+	constructor(private axios: AxiosInstance) {}
+
+	async postPlan(req: PostPlanRequest) {
+		const { data } = await this.axios.post<Plan>('/api/plan/time', { ...req });
+
+		return data;
+	}
+
+	async deletePlan(planTimeId: number) {
+		const { data } = await this.axios.delete<Plan>(
+			'/api/plan/time/' + planTimeId
+		);
+
+		return data;
+	}
+
+	async getPlanPages() {
+		const { data } = await this.axios.get<PlanPage[]>('/api/plan/page');
+
+		return data;
+	}
+
+	async getPlanPage(planPageId: number) {
+		const { data } = await this.axios.get<PlanPageResponse>(
+			'/api/plan/page/' + planPageId
+		);
+
+		return data;
+	}
+
+	async postCreatePlanPage(name: string) {
+		const { data } = await this.axios.post<PlanPageResponse>('/api/plan/page', {
+			name,
+		});
+
+		return data;
+	}
+
+	async patchModifyPlanPage({
+		planPageId,
+		name,
+	}: {
+		planPageId: number;
+		name: string;
+	}) {
+		const { data } = await this.axios.patch<PlanPageResponse>(
+			'/api/plan/page/' + planPageId,
+			{
+				name,
+			}
+		);
+
+		return data;
+	}
+
+	async deletePlanPage(planPageId: number) {
+		const { data } = await this.axios.delete<PlanPageResponse[]>(
+			'/api/plan/page/' + planPageId
+		);
+
+		return data;
+	}
+}
