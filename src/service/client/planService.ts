7de63a54978b14e7bb64af8b49746c13f201b9c7
SeungYn <kwls0407@gmail.com>
2024-08-30 23:00:08 +0900

    feat-client: 계획 수정 service 코드 추가

--- src/service/client/planService.ts
+++ src/service/client/planService.ts
@@ -1,5 +1,9 @@
 import { AxiosInstance } from 'axios';
-import { PlanPageResponse, PostPlanRequest } from '../types/plan';
+import {
+	PatchPlanRequest,
+	PlanPageResponse,
+	PostPlanRequest,
+} from '../types/plan';
 import { Plan, PlanPage } from '@prisma/client';
 
 export default class PlanService {
@@ -7,6 +11,14 @@ export default class PlanService {
 
 	async postPlan(req: PostPlanRequest) {
 		const { data } = await this.axios.post<Plan>('/api/plan/time', { ...req });
+
+		return data;
+	}
+
+	async patchUpdatePlan(req: PatchPlanRequest) {
+		const url = `/api/plan/time/${req.planId}`;
+
+		const { data } = await this.axios.patch<Plan>(url, { ...req });
 
 		return data;
 	}
