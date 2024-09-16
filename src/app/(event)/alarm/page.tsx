f62e6c54502acb69356be2054938587d340f3259
SeungYn <kwls0407@gmail.com>
2024-09-16 21:20:37 +0900

    feat: 설명 페이지 추가

--- src/app/(event)/alarm/page.tsx
+++ src/app/(event)/alarm/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function AlarmGuidePage() {
 	return (
@@ -14,24 +15,96 @@ export default function AlarmGuidePage() {
 				</div>
 			</header>
 			<section className="mx-auto my-10 w-full max-w-7xl">
-				<section className="rounded-lg bg-h_light_black p-4">
-					<h1 className="text-3xl font-bold">Chrome 앱 PC 설치 방법 </h1>
+				<section className="mb-10 rounded-lg bg-h_light_black p-4">
+					<Link
+						href={
+							'https://support.google.com/chrome/answer/3220216?hl=ko&co=GENIE.Platform%3DDesktop&oco=1'
+						}
+						className="text-3xl font-bold"
+						target="_blank"
+					>
+						Chrome 앱 PC 알림 설정 방법 바로가기
+					</Link>
+					<hr className="mb-8" />
+					<Image
+						src={'/event/pwa/chrome-alarm-pc.png'}
+						alt="Picture of the author"
+						sizes="(min-width: 600px) 30vw, 100vw"
+						style={{
+							width: '100%',
+							height: 'auto',
+							aspectRatio: '1/1.7',
+						}}
+						width={300}
+						height={450}
+					/>
+				</section>
+				<section className="mb-10 rounded-lg bg-h_light_black p-4">
+					<Link
+						href={
+							'https://support.google.com/chrome/answer/3220216?hl=ko&co=GENIE.Platform%3DAndroid&oco=1'
+						}
+						target="_blank"
+						className="text-3xl font-bold"
+					>
+						Chrome 앱 안드로이드 알림 설정 방법 바로가기
+					</Link>
+					<hr className="mb-8" />
+					<Image
+						src={'/event/pwa/chrome-alarm-and.png'}
+						alt="Picture of the author"
+						sizes="(min-width: 600px) 30vw, 100vw"
+						style={{
+							width: '100%',
+							height: 'auto',
+							aspectRatio: '1/1.7',
+						}}
+						width={300}
+						height={450}
+					/>
+				</section>
+				<section className="mb-10 rounded-lg bg-h_light_black p-4">
+					<Link
+						href={
+							'https://support.google.com/chrome/answer/3220216?hl=ko&co=GENIE.Platform%3DiOS&oco=1'
+						}
+						target="_blank"
+						className="text-3xl font-bold"
+					>
+						Chrome 앱 IOS 알림 설정 방법 바로가기
+					</Link>
+					<hr className="mb-8" />
+					<Image
+						src={'/event/pwa/chrome-alarm-ios.png'}
+						alt="Picture of the author"
+						sizes="(min-width: 600px) 30vw, 100vw"
+						style={{
+							width: '100%',
+							height: 'auto',
+							aspectRatio: '1/1.7',
+						}}
+						width={300}
+						height={450}
+					/>
+				</section>
+				<section className="mb-10 rounded-lg bg-h_light_black p-4">
+					<Link
+						href={'https://support.apple.com/ko-kr/guide/safari/sfri40734/mac'}
+						className="text-3xl font-bold"
+						target="_blank"
+					>
+						Safari 앱 PC 알림 설정 방법 바로가기
+					</Link>
 					<hr />
 				</section>
-				<section className="bg-h_light_black">
-					<h1>Chrome 앱 안드로이드 설치 방법 </h1>
-					<hr />
-				</section>
-				<section className="bg-h_light_black">
-					<h1>Chrome 앱 IOS 설치 방법 </h1>
-					<hr />
-				</section>
-				<section className="bg-h_light_black">
-					<h1>Safari 앱 PC 설치 방법 </h1>
-					<hr />
-				</section>
-				<section className="bg-h_light_black">
-					<h1>Safari 앱 IOS 설치 방법 </h1>
+				<section className="mb-10 rounded-lg bg-h_light_black p-4">
+					<Link
+						href={'https://support.apple.com/ko-kr/guide/safari/sfri40734/mac'}
+						className="text-3xl font-bold"
+						target="_blank"
+					>
+						Safari 앱 IOS 알림 설정 방법 바로가기
+					</Link>
 					<hr />
 				</section>
 			</section>
