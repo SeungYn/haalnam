16fcb238974458f213e5961c4c6b45b39948416d
SeungYn <kwls0407@gmail.com>
2024-10-28 23:15:55 +0900

    feat: d3 툴팁 class 추가 및 주간 분석 버튼 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,21 +1,23 @@
 import { IntroLogin, ReduceTimer } from '@/components/home';
+import YesterdayAnalyzePopupContainer from '@/container/analyze/YesterdayAnalyzePopupContainer';
 import { auth } from '@/lib/auth';
 
 export default async function Home() {
-  const session = await auth();
-  const user = session?.user;
+	const session = await auth();
+	const user = session?.user;
 
-  return (
-    <div className='w-full h-full flex items-center flex-col'>
-      <h1 className={`text-5xl text-neutral-100 mt-8 text-center font-bold `}>
-        하루를 <strong className='text-red-300'>어떻게</strong> <br />{' '}
-        사용하셨나요?
-      </h1>
-      <ReduceTimer />
-      {!user && <IntroLogin />}
-      {user && (
-        <h2 className='text-4xl text-white mt-4'>{`안녕하세요! ${user.name}님!`}</h2>
-      )}
-    </div>
-  );
+	return (
+		<div className="flex h-full w-full flex-col items-center">
+			<h1 className={`mt-8 text-center text-5xl font-bold text-neutral-100`}>
+				하루를 <strong className="text-red-300">어떻게</strong> <br />{' '}
+				사용하셨나요?
+			</h1>
+			<ReduceTimer />
+			{!user && <IntroLogin />}
+			{user && (
+				<h2 className="mt-4 text-4xl text-white">{`안녕하세요! ${user.name}님!`}</h2>
+			)}
+			{user && <YesterdayAnalyzePopupContainer />}
+		</div>
+	);
 }
